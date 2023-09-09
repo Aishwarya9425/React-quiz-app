08d724a816433969bf6dfb10b2a37205c70b0731
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-09-10 00:22:22 +0530

    updated fetch api url to json server

--- src/components/App.js
+++ src/components/App.js
@@ -99,7 +99,7 @@ export default function App() {
   );
 
   useEffect(function () {
-    fetch("http://localhost:9000/questions")
+    fetch("https://json-server-react-quiz.vercel.app/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
